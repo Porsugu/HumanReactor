cedfbdae2ce6b194bb9479eb9382eb5c78e33b20
Course test <joycelyntsang@gmail.com>
2025-04-27 22:16:54 -0700

    deleted the unwanted materials

--- app/src/main/java/com/example/humanreactor/aiQuestion/QuizQuestion.kt
+++
@@ -1,9 +0,0 @@
-package com.example.humanreactor.aiQuestion
-
-// this is the data class for quiz question after generated
-data class QuizQuestion(
-    var question: String,
-    val options: List<String>,
-    val correctAnswerIndex: String,
-    val explanation: String
-)
